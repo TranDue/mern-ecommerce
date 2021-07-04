ddd736b6db9aabc714b01e99a5e552f611f67630
Tran Due <tran.congdue1999@gmail.com>
2021-07-04 19:05:08 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "https://due-ecommerce.herokuapp.com/",
+        "BASE_URL": "https://mern-ecommerce-ecru.vercel.app/",
         "MONGODB_URL": "mongodb+srv://duetran:215437331@cluster0.lvnqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
         "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
